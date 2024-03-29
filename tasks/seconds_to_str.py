b446d3297ebb116295e36e166ad573d3d5c86896
Aleks3188 <chernovol_a.v@mail.ru>
2024-03-29 16:49:59 +0300

    win-second_to_str

--- tasks/seconds_to_str.py
+++ tasks/seconds_to_str.py
@@ -1,9 +1,22 @@
 __all__ = ("seconds_to_str",)
 
-import time
-
-
 def seconds_to_str(seconds: int) -> str:
-    time_convert = time.strftime("%Dd%Hh%Mm%Ss", time.gmtime(seconds))
-    print(time_convert)
-    raise NotImplementedError
+    days_value = seconds // (24 * 60 * 60)
+    seconds -= days_value * (24 * 60 * 60)
+    hours = seconds // (60 * 60)
+    seconds -= hours * (60 * 60)
+    minutes = seconds // 60
+    seconds -= minutes * 60
+    result = []
+    if days_value > 0:
+        result.append(f'{"%02d" % (days_value)}d')
+    if hours > 0 or result:
+        result.append(f'{"%02d" % (hours)}h')
+    if minutes > 0 or result:
+        result.append(f'{"%02d" % (minutes)}m')
+    if seconds > 0 or result:
+        result.append(f'{"%02d" % (seconds)}s')
+    else:
+        if seconds == 0:
+            return (f'00s')
+    return ''.join(result)

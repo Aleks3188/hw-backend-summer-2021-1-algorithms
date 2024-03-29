b4ecfc1ed7bb36482f655146c2003aa7e384c9e9
Aleks3188 <chernovol_a.v@mail.ru>
2024-03-29 09:48:33 +0300

    secont_to_str

--- tasks/seconds_to_str.py
+++ tasks/seconds_to_str.py
@@ -1,19 +1,9 @@
 __all__ = ("seconds_to_str",)
 
+import time
+
 
 def seconds_to_str(seconds: int) -> str:
-    """Реализует текстовое представление времени.
-
-    Example:
-        >> seconds_to_str(20)
-        20s
-        >> seconds_to_str(60)
-        01m00s
-        >> seconds_to_str(65)
-        01m05s
-        >> seconds_to_str(3700)
-        01h01m40s
-        >> seconds_to_str(93600)
-        01d02h00m00s
-    """
+    time_convert = time.strftime("%Dd%Hh%Mm%Ss", time.gmtime(seconds))
+    print(time_convert)
     raise NotImplementedError

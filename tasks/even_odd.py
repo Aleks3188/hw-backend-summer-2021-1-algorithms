807a9364a0fd202132df2865ac987af67851bfc5
Aleks3188 <chernovol_a.v@mail.ru>
2024-03-30 12:17:01 +0300

    win_even_odd.py

--- tasks/even_odd.py
+++ tasks/even_odd.py
@@ -2,11 +2,12 @@ __all__ = ("even_odd",)
 
 
 def even_odd(numbers: list[int]) -> float:
-    """Определяет отношение суммы четных элементов списка
-    к сумме нечетных.
-
-    Example:
-        >> even_odd([1, 2, 3, 4, 5])
-        0.6667
-    """
-    raise NotImplementedError
+    chet = []
+    nechet = [] 
+    for index in numbers:
+        if index % 2 == 0:
+            chet.append(index)
+        if index % 2 != 0:
+            nechet.append(index)
+    sum_final = sum(chet) / sum(nechet)
+    return sum_final

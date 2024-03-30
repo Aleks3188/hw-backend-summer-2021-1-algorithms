e51f56903becb68cf74b9e3418d14f01ba157adf
Aleks3188 <chernovol_a.v@mail.ru>
2024-03-30 11:45:10 +0300

    win_is_prime

--- tasks/is_prime.py
+++ tasks/is_prime.py
@@ -2,14 +2,16 @@ __all__ = ("is_prime",)
 
 
 def is_prime(number: int) -> bool:
-    """Определяет, является ли число простым.
-
-    Example:
-        >> is_prime(0):
-        False
-        >> is_prime(1):
-        False
-        >> is_prime(4):
-        True
-    """
-    raise NotImplementedError
+    if number <= 1:
+        return False
+    if number <= 3 and number > 1:
+        return True
+    if number % 2 == 0 or number % 3 == 0:
+        return False
+    index_number = 5
+    while index_number * index_number <= number:
+        if number % index_number == 0 or number % (index_number + 2) == 0:
+            return False
+        index_number += 6
+    return True
+print(is_prime(5))

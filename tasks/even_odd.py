105748834683d54e726357c984949e9b07663d39
Aleks3188 <chernovol_a.v@mail.ru>
2024-04-01 10:30:39 +0300

    win-even_odd

--- tasks/even_odd.py
+++ tasks/even_odd.py
@@ -9,5 +9,9 @@ def even_odd(numbers: list[int]) -> float:
             chet.append(index)
         if index % 2 != 0:
             nechet.append(index)
-    sum_final = sum(chet) / sum(nechet)
-    return sum_final
+    if sum(chet) > 0 and sum(nechet) > 0:
+        sum_final = sum(chet) / sum(nechet)
+        return sum_final
+    else:
+        return 0
+
